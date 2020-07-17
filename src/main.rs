3ccdece4fd8519995afcc9c59e2190b560b91040
Saulo Oliveira <me@sauloefo.com>
2020-07-17 21:34:20 +0100

    adding task struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+struct Task {
+    description: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let task1 = Task {
+        description: String::from("Task 1")
+    };
+
+    let task2 = Task {
+        description: String::from("Task 2")
+    };
+
+    let my_tasks = [
+        task1,
+        task2
+    ];
+
+    for task in my_tasks.iter() {
+        println!("{}", task.description);
+    }
 }
